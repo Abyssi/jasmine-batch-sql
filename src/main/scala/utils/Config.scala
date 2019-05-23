ab7ea704756e06932c24f0eb71e449648a33ac59
Andrea Silvi <andrea.silvi94@gmail.com>
2019-05-23 13:41:50 +0200

    Added config

---
+++ src/main/scala/utils/Config.scala
@@ -0,0 +1,26 @@
+package utils
+
+case class Config(inputBasePath: String, outputBasePath: String, inputFormat: String, clearCitiesQueryEnabled: Boolean, countryMetricsQueryEnabled: Boolean, maxDiffCountriesQueryEnabled: Boolean)
+
+object Config {
+  def parseArgs(args: Array[String]): Config = args.sliding(2, 1).toList.foldLeft(this.default) {
+    case (accumArgs, currArgs) => currArgs match {
+      case Array("--input-base-path", inputBasePath) => accumArgs.copy(inputBasePath = inputBasePath)
+      case Array("--output-base-path", outputBasePath) => accumArgs.copy(outputBasePath = outputBasePath)
+      case Array("--input-format", inputFormat) => accumArgs.copy(inputFormat = inputFormat)
+      case Array("--clear-cities-query-enabled", clearCitiesQueryEnabled) => accumArgs.copy(clearCitiesQueryEnabled = clearCitiesQueryEnabled.toBoolean)
+      case Array("--country-metrics-query-enabled", countryMetricsQueryEnabled) => accumArgs.copy(countryMetricsQueryEnabled = countryMetricsQueryEnabled.toBoolean)
+      case Array("--max-diff-countries-query-enabled", maxDiffCountriesQueryEnabled) => accumArgs.copy(maxDiffCountriesQueryEnabled = maxDiffCountriesQueryEnabled.toBoolean)
+      case _ => accumArgs // Do whatever you want for this case
+    }
+  }
+
+  def default = new Config(
+    "data/inputs/processed/",
+    "data/outputs/sql/",
+    "parquet",
+    true,
+    false,
+    false
+  )
+}
