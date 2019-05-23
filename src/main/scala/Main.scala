4314caa0aded5884d8cac078eeea6875d1521edd
Andrea Silvi <andrea.silvi94@gmail.com>
2019-05-24 00:10:49 +0200

    Added config for removing initial join and refactored models

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,8 @@
-import model.{CountryCityRankCompareItemParser, YearCityItemParser, YearMonthCountryMetricsItemParser}
+import model._
 import operators.SQLQueryBuilder
 import org.apache.spark.sql.SparkSession
 import queries.{ClearCitiesQuery, CountryMetricsQuery, MaxDiffCountriesQuery}
-import utils.{Config, ProfilingUtils}
+import utils.Config
 
 object Main {
 
@@ -22,7 +22,7 @@ object Main {
     val config = Config.parseArgs(args)
 
     if (config.clearCitiesQueryEnabled || config.countryMetricsQueryEnabled || config.maxDiffCountriesQueryEnabled) {
-      spark.read.parquet(s"${config.inputBasePath}${config.inputFormat}/city_attributes.${config.inputFormat}")
+      if (config.needJoin) spark.read.parquet(s"${config.inputBasePath}${config.inputFormat}/city_attributes.${config.inputFormat}")
         .createOrReplaceTempView("attributes")
 
       // CLEAR CITIES QUERY
@@ -32,59 +32,61 @@ object Main {
         val weatherDescriptionInput = new SQLQueryBuilder(spark, "weather_description")
           .sql("weather_description", "SELECT TO_TIMESTAMP(REPLACE(table.datetime, ' ', 'T') || attributes.timeOffset, \"yyyy-MM-dd'T'HH:mm:ssZ\") as datetime, table.city, attributes.country, table.value FROM {TABLE_NAME} AS table INNER JOIN attributes ON table.city=attributes.City")
 
-        val clearCitiesOutputPath = config.outputBasePath + "sql/clear_cities"
+        val clearCitiesOutputPath = config.outputBasePath + "clear_cities"
         val clearCitiesOutput = ClearCitiesQuery.run(weatherDescriptionInput)
-        ProfilingUtils.timeDataFrame(clearCitiesOutput, "clear Cities Output")
-
-        clearCitiesOutput.show()
-        clearCitiesOutput.rdd.map(YearCityItemParser.FromRow).saveAsTextFile(clearCitiesOutputPath)
+        //ProfilingUtils.timeDataFrame(clearCitiesOutput, "clear Cities Output")
+        //clearCitiesOutput.show()
+        clearCitiesOutput.rdd.map(YearCityOutputItem.From).saveAsTextFile(clearCitiesOutputPath)
       }
 
       if (config.countryMetricsQueryEnabled || config.maxDiffCountriesQueryEnabled) {
         spark.read.parquet(s"${config.inputBasePath}${config.inputFormat}/temperature.${config.inputFormat}")
           .createOrReplaceTempView("temperature")
-        val temperatureInput = new SQLQueryBuilder(spark, "temperature")
-          .sql("temperature", "SELECT TO_TIMESTAMP(REPLACE(table.datetime, ' ', 'T') || attributes.timeOffset, \"yyyy-MM-dd'T'HH:mm:ssZ\") as datetime, table.city, attributes.country, table.value FROM {TABLE_NAME} AS table INNER JOIN attributes ON table.city=attributes.City")
+        var temperatureInput = new SQLQueryBuilder(spark, "temperature")
+        if (config.needJoin)
+          temperatureInput = temperatureInput.sql("temperature", "SELECT TO_TIMESTAMP(REPLACE(table.datetime, ' ', 'T') || attributes.timeOffset, \"yyyy-MM-dd'T'HH:mm:ssZ\") as datetime, table.city, attributes.country, table.value FROM {TABLE_NAME} AS table INNER JOIN attributes ON table.city=attributes.City")
 
         // COUNTRY METRICS QUERY
         if (config.countryMetricsQueryEnabled) {
           spark.read.parquet(s"${config.inputBasePath}${config.inputFormat}/humidity.${config.inputFormat}")
             .createOrReplaceTempView("humidity")
-          val humidityInput = new SQLQueryBuilder(spark, "humidity")
-            .sql("humidity", "SELECT TO_TIMESTAMP(REPLACE(table.datetime, ' ', 'T') || attributes.timeOffset, \"yyyy-MM-dd'T'HH:mm:ssZ\") as datetime, table.city, attributes.country, table.value FROM {TABLE_NAME} AS table INNER JOIN attributes ON table.city=attributes.City")
+          var humidityInput = new SQLQueryBuilder(spark, "humidity")
+          if (config.needJoin)
+            humidityInput = humidityInput.sql("humidity", "SELECT TO_TIMESTAMP(REPLACE(table.datetime, ' ', 'T') || attributes.timeOffset, \"yyyy-MM-dd'T'HH:mm:ssZ\") as datetime, table.city, attributes.country, table.value FROM {TABLE_NAME} AS table INNER JOIN attributes ON table.city=attributes.City")
 
-          spark.read.parquet(s"${config.inputBasePath}${config.inputFormat}/humidity.${config.inputFormat}")
-            .createOrReplaceTempView("humidity")
-          val pressureInput = new SQLQueryBuilder(spark, "pressure")
-            .sql("pressure", "SELECT TO_TIMESTAMP(REPLACE(table.datetime, ' ', 'T') || attributes.timeOffset, \"yyyy-MM-dd'T'HH:mm:ssZ\") as datetime, table.city, attributes.country, table.value FROM {TABLE_NAME} AS table INNER JOIN attributes ON table.city=attributes.City")
+          spark.read.parquet(s"${config.inputBasePath}${config.inputFormat}/pressure.${config.inputFormat}")
+            .createOrReplaceTempView("pressure")
+          var pressureInput = new SQLQueryBuilder(spark, "pressure")
+          if (config.needJoin)
+            pressureInput = pressureInput.sql("pressure", "SELECT TO_TIMESTAMP(REPLACE(table.datetime, ' ', 'T') || attributes.timeOffset, \"yyyy-MM-dd'T'HH:mm:ssZ\") as datetime, table.city, attributes.country, table.value FROM {TABLE_NAME} AS table INNER JOIN attributes ON table.city=attributes.City")
 
           val humidityCountryMetricsOutputPath = config.outputBasePath + "humidity_country_metrics"
           val humidityCountryMetricsOutput = CountryMetricsQuery.run(humidityInput)
-          humidityCountryMetricsOutput.show()
-          humidityCountryMetricsOutput.rdd.map(YearMonthCountryMetricsItemParser.FromRow).saveAsTextFile(humidityCountryMetricsOutputPath)
+          //humidityCountryMetricsOutput.show()
+          humidityCountryMetricsOutput.rdd.map(YearMonthCountryMetricsOutputItem.From).saveAsTextFile(humidityCountryMetricsOutputPath)
 
           val pressureCountryMetricsOutputPath = config.outputBasePath + "pressure_country_metrics"
           val pressureCountryMetricsOutput = CountryMetricsQuery.run(pressureInput)
-          pressureCountryMetricsOutput.show()
-          pressureCountryMetricsOutput.rdd.map(YearMonthCountryMetricsItemParser.FromRow).saveAsTextFile(pressureCountryMetricsOutputPath)
+          //pressureCountryMetricsOutput.show()
+          pressureCountryMetricsOutput.rdd.map(YearMonthCountryMetricsOutputItem.From).saveAsTextFile(pressureCountryMetricsOutputPath)
 
           val temperatureCountryMetricsOutputPath = config.outputBasePath + "temperature_country_metrics"
           val temperatureCountryMetricsOutput = CountryMetricsQuery.run(temperatureInput)
-          temperatureCountryMetricsOutput.show(false)
-          temperatureCountryMetricsOutput.rdd.map(YearMonthCountryMetricsItemParser.FromRow).saveAsTextFile(temperatureCountryMetricsOutputPath)
+          //temperatureCountryMetricsOutput.show(false)
+          temperatureCountryMetricsOutput.rdd.map(YearMonthCountryMetricsOutputItem.From).saveAsTextFile(temperatureCountryMetricsOutputPath)
         }
 
         // MAX DIFF COUNTRIES QUERY
         if (config.maxDiffCountriesQueryEnabled) {
           val maxDiffCountriesOutputPath = config.outputBasePath + "max_diff_countries"
           val maxDiffCountriesOutput = MaxDiffCountriesQuery.run(temperatureInput)
-          maxDiffCountriesOutput.show(false)
-          maxDiffCountriesOutput.rdd.map(CountryCityRankCompareItemParser.FromRow).saveAsTextFile(maxDiffCountriesOutputPath)
+          //maxDiffCountriesOutput.show(false)
+          maxDiffCountriesOutput.rdd.map(CountryCityRankCompareOutputItem.From).saveAsTextFile(maxDiffCountriesOutputPath)
         }
       }
     }
 
-    System.in.read
+    //System.in.read
     spark.stop()
   }
 
--- src/main/scala/utils/Config.scala
+++ src/main/scala/utils/Config.scala
@@ -1,6 +1,6 @@
 package utils
 
-case class Config(inputBasePath: String, outputBasePath: String, inputFormat: String, clearCitiesQueryEnabled: Boolean, countryMetricsQueryEnabled: Boolean, maxDiffCountriesQueryEnabled: Boolean)
+case class Config(inputBasePath: String, outputBasePath: String, inputFormat: String, needJoin: Boolean, clearCitiesQueryEnabled: Boolean, countryMetricsQueryEnabled: Boolean, maxDiffCountriesQueryEnabled: Boolean)
 
 object Config {
   def parseArgs(args: Array[String]): Config = args.sliding(2, 1).toList.foldLeft(this.default) {
@@ -8,6 +8,7 @@ object Config {
       case Array("--input-base-path", inputBasePath) => accumArgs.copy(inputBasePath = inputBasePath)
       case Array("--output-base-path", outputBasePath) => accumArgs.copy(outputBasePath = outputBasePath)
       case Array("--input-format", inputFormat) => accumArgs.copy(inputFormat = inputFormat)
+      case Array("--need-join", needJoin) => accumArgs.copy(needJoin = needJoin.toBoolean)
       case Array("--clear-cities-query-enabled", clearCitiesQueryEnabled) => accumArgs.copy(clearCitiesQueryEnabled = clearCitiesQueryEnabled.toBoolean)
       case Array("--country-metrics-query-enabled", countryMetricsQueryEnabled) => accumArgs.copy(countryMetricsQueryEnabled = countryMetricsQueryEnabled.toBoolean)
       case Array("--max-diff-countries-query-enabled", maxDiffCountriesQueryEnabled) => accumArgs.copy(maxDiffCountriesQueryEnabled = maxDiffCountriesQueryEnabled.toBoolean)
@@ -20,7 +21,8 @@ object Config {
     "data/outputs/sql/",
     "parquet",
     true,
-    false,
-    false
+    true,
+    true,
+    true
   )
 }
