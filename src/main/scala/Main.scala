d2d6fa677c0060d7ac32e9cf693559131cad2886
Andrea Silvi <andrea.silvi94@gmail.com>
2019-05-23 13:40:54 +0200

    Added config and profiling

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,7 @@ import model.{CountryCityRankCompareItemParser, YearCityItemParser, YearMonthCou
 import operators.SQLQueryBuilder
 import org.apache.spark.sql.SparkSession
 import queries.{ClearCitiesQuery, CountryMetricsQuery, MaxDiffCountriesQuery}
+import utils.{Config, ProfilingUtils}
 
 object Main {
 
@@ -13,62 +14,77 @@ object Main {
   def main(args: Array[String]) {
     val spark = SparkSession
       .builder()
-      .master("local")
-      .appName("JASMINE")
+      .master("local[*]")
+      .appName("JASMINE Batch SQL")
       .config("spark.hadoop.validateOutputSpecs", "false")
       .getOrCreate()
 
-    val inputBasePath = "data/inputs/processed/"
-    val outputBasePath = "data/outputs/sql/"
+    val config = Config.parseArgs(args)
 
-    spark.read.parquet(inputBasePath + "parquet/city_attributes.parquet")
-      .createOrReplaceTempView("attributes")
+    if (config.clearCitiesQueryEnabled || config.countryMetricsQueryEnabled || config.maxDiffCountriesQueryEnabled) {
+      spark.read.parquet(s"${config.inputBasePath}${config.inputFormat}/city_attributes.${config.inputFormat}")
+        .createOrReplaceTempView("attributes")
 
-    spark.read.parquet(inputBasePath + "parquet/humidity.parquet")
-      .createOrReplaceTempView("humidity")
-    val humidityInput = new SQLQueryBuilder(spark, "humidity")
-      .sql("humidity", "SELECT TO_TIMESTAMP(REPLACE(table.datetime, ' ', 'T') || attributes.timeOffset, \"yyyy-MM-dd'T'HH:mm:ssZ\") as datetime, table.city, attributes.country, table.value FROM {TABLE_NAME} AS table INNER JOIN attributes ON table.city=attributes.City")
+      // CLEAR CITIES QUERY
+      if (config.clearCitiesQueryEnabled) {
+        spark.read.parquet(s"${config.inputBasePath}${config.inputFormat}/weather_description.${config.inputFormat}")
+          .createOrReplaceTempView("weather_description")
+        val weatherDescriptionInput = new SQLQueryBuilder(spark, "weather_description")
+          .sql("weather_description", "SELECT TO_TIMESTAMP(REPLACE(table.datetime, ' ', 'T') || attributes.timeOffset, \"yyyy-MM-dd'T'HH:mm:ssZ\") as datetime, table.city, attributes.country, table.value FROM {TABLE_NAME} AS table INNER JOIN attributes ON table.city=attributes.City")
 
-    spark.read.parquet(inputBasePath + "parquet/pressure.parquet")
-      .createOrReplaceTempView("pressure")
-    val pressureInput = new SQLQueryBuilder(spark, "pressure")
-      .sql("pressure", "SELECT TO_TIMESTAMP(REPLACE(table.datetime, ' ', 'T') || attributes.timeOffset, \"yyyy-MM-dd'T'HH:mm:ssZ\") as datetime, table.city, attributes.country, table.value FROM {TABLE_NAME} AS table INNER JOIN attributes ON table.city=attributes.City")
+        val clearCitiesOutputPath = config.outputBasePath + "sql/clear_cities"
+        val clearCitiesOutput = ClearCitiesQuery.run(weatherDescriptionInput)
+        ProfilingUtils.timeDataFrame(clearCitiesOutput, "clear Cities Output")
 
-    spark.read.parquet(inputBasePath + "parquet/temperature.parquet")
-      .createOrReplaceTempView("temperature")
-    val temperatureInput = new SQLQueryBuilder(spark, "temperature")
-      .sql("temperature", "SELECT TO_TIMESTAMP(REPLACE(table.datetime, ' ', 'T') || attributes.timeOffset, \"yyyy-MM-dd'T'HH:mm:ssZ\") as datetime, table.city, attributes.country, table.value FROM {TABLE_NAME} AS table INNER JOIN attributes ON table.city=attributes.City")
+        clearCitiesOutput.show()
+        clearCitiesOutput.rdd.map(YearCityItemParser.FromRow).saveAsTextFile(clearCitiesOutputPath)
+      }
 
-    spark.read.parquet(inputBasePath + "parquet/weather_description.parquet")
-      .createOrReplaceTempView("weather_description")
-    val weatherDescriptionInput = new SQLQueryBuilder(spark, "weather_description")
-      .sql("weather_description", "SELECT TO_TIMESTAMP(REPLACE(table.datetime, ' ', 'T') || attributes.timeOffset, \"yyyy-MM-dd'T'HH:mm:ssZ\") as datetime, table.city, attributes.country, table.value FROM {TABLE_NAME} AS table INNER JOIN attributes ON table.city=attributes.City")
+      if (config.countryMetricsQueryEnabled || config.maxDiffCountriesQueryEnabled) {
+        spark.read.parquet(s"${config.inputBasePath}${config.inputFormat}/temperature.${config.inputFormat}")
+          .createOrReplaceTempView("temperature")
+        val temperatureInput = new SQLQueryBuilder(spark, "temperature")
+          .sql("temperature", "SELECT TO_TIMESTAMP(REPLACE(table.datetime, ' ', 'T') || attributes.timeOffset, \"yyyy-MM-dd'T'HH:mm:ssZ\") as datetime, table.city, attributes.country, table.value FROM {TABLE_NAME} AS table INNER JOIN attributes ON table.city=attributes.City")
 
-    val clearCitiesOutputPath = outputBasePath + "sql/clear_cities"
-    val clearCitiesOutput = ClearCitiesQuery.run(weatherDescriptionInput)
-    clearCitiesOutput.show()
-    clearCitiesOutput.rdd.map(YearCityItemParser.FromRow).saveAsTextFile(clearCitiesOutputPath)
+        // COUNTRY METRICS QUERY
+        if (config.countryMetricsQueryEnabled) {
+          spark.read.parquet(s"${config.inputBasePath}${config.inputFormat}/humidity.${config.inputFormat}")
+            .createOrReplaceTempView("humidity")
+          val humidityInput = new SQLQueryBuilder(spark, "humidity")
+            .sql("humidity", "SELECT TO_TIMESTAMP(REPLACE(table.datetime, ' ', 'T') || attributes.timeOffset, \"yyyy-MM-dd'T'HH:mm:ssZ\") as datetime, table.city, attributes.country, table.value FROM {TABLE_NAME} AS table INNER JOIN attributes ON table.city=attributes.City")
 
-    val humidityCountryMetricsOutputPath = outputBasePath + "humidity_country_metrics"
-    val humidityCountryMetricsOutput = CountryMetricsQuery.run(humidityInput)
-    humidityCountryMetricsOutput.show()
-    humidityCountryMetricsOutput.rdd.map(YearMonthCountryMetricsItemParser.FromRow).saveAsTextFile(humidityCountryMetricsOutputPath)
+          spark.read.parquet(s"${config.inputBasePath}${config.inputFormat}/humidity.${config.inputFormat}")
+            .createOrReplaceTempView("humidity")
+          val pressureInput = new SQLQueryBuilder(spark, "pressure")
+            .sql("pressure", "SELECT TO_TIMESTAMP(REPLACE(table.datetime, ' ', 'T') || attributes.timeOffset, \"yyyy-MM-dd'T'HH:mm:ssZ\") as datetime, table.city, attributes.country, table.value FROM {TABLE_NAME} AS table INNER JOIN attributes ON table.city=attributes.City")
 
-    val pressureCountryMetricsOutputPath = outputBasePath + "pressure_country_metrics"
-    val pressureCountryMetricsOutput = CountryMetricsQuery.run(pressureInput)
-    pressureCountryMetricsOutput.show()
-    pressureCountryMetricsOutput.rdd.map(YearMonthCountryMetricsItemParser.FromRow).saveAsTextFile(pressureCountryMetricsOutputPath)
+          val humidityCountryMetricsOutputPath = config.outputBasePath + "humidity_country_metrics"
+          val humidityCountryMetricsOutput = CountryMetricsQuery.run(humidityInput)
+          humidityCountryMetricsOutput.show()
+          humidityCountryMetricsOutput.rdd.map(YearMonthCountryMetricsItemParser.FromRow).saveAsTextFile(humidityCountryMetricsOutputPath)
 
-    val temperatureCountryMetricsOutputPath = outputBasePath + "temperature_country_metrics"
-    val temperatureCountryMetricsOutput = CountryMetricsQuery.run(temperatureInput)
-    temperatureCountryMetricsOutput.show(false)
-    temperatureCountryMetricsOutput.rdd.map(YearMonthCountryMetricsItemParser.FromRow).saveAsTextFile(temperatureCountryMetricsOutputPath)
+          val pressureCountryMetricsOutputPath = config.outputBasePath + "pressure_country_metrics"
+          val pressureCountryMetricsOutput = CountryMetricsQuery.run(pressureInput)
+          pressureCountryMetricsOutput.show()
+          pressureCountryMetricsOutput.rdd.map(YearMonthCountryMetricsItemParser.FromRow).saveAsTextFile(pressureCountryMetricsOutputPath)
 
-    val maxDiffCountriesOutputPath = outputBasePath + "max_diff_countries"
-    val maxDiffCountriesOutput = MaxDiffCountriesQuery.run(temperatureInput)
-    maxDiffCountriesOutput.show(false)
-    maxDiffCountriesOutput.rdd.map(CountryCityRankCompareItemParser.FromRow).saveAsTextFile(maxDiffCountriesOutputPath)
+          val temperatureCountryMetricsOutputPath = config.outputBasePath + "temperature_country_metrics"
+          val temperatureCountryMetricsOutput = CountryMetricsQuery.run(temperatureInput)
+          temperatureCountryMetricsOutput.show(false)
+          temperatureCountryMetricsOutput.rdd.map(YearMonthCountryMetricsItemParser.FromRow).saveAsTextFile(temperatureCountryMetricsOutputPath)
+        }
 
+        // MAX DIFF COUNTRIES QUERY
+        if (config.maxDiffCountriesQueryEnabled) {
+          val maxDiffCountriesOutputPath = config.outputBasePath + "max_diff_countries"
+          val maxDiffCountriesOutput = MaxDiffCountriesQuery.run(temperatureInput)
+          maxDiffCountriesOutput.show(false)
+          maxDiffCountriesOutput.rdd.map(CountryCityRankCompareItemParser.FromRow).saveAsTextFile(maxDiffCountriesOutputPath)
+        }
+      }
+    }
+
+    System.in.read
     spark.stop()
   }
 
